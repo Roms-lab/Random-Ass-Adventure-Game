47a89299b6e87666b4803bbf96e89d1e23edaab9
Zach <100164907@ccisd.net>
2025-08-20 16:46:36 -0500

    Add more content :o

--- Adventure Game.cpp
+++ Adventure Game.cpp
@@ -8,6 +8,30 @@ int main() {
 	cin >> Sub;
 	if (Sub == 1) {
 		cout << "You found sticks :o" << endl;
+		int Sub2;
+		cout << "1: Sharpen sticks, 2: Look for food" << endl;
+		cin >> Sub2;
+		if (Sub2 == 1) {
+			cout << "You sharpened stick :o" << endl;
+			int Sub3;
+			cout << "1: Find food, 2: Drown :>" << endl;
+			cin >> Sub3;
+			if (Sub3 == 1) {
+				cout << "You kill baby deer :D" << endl;
+			}
+			else if (Sub3 == 2) {
+				cout << "You died..." << endl;
+			}
+			else {
+				cout << "Invalid option!" << endl;
+			}
+		}
+		else if (Sub2 == 2) {
+			cout << "You get mauled to death by a bear" << endl;
+		}
+		else {
+			cout << "Invalid option!" << endl;
+		}
 	}
 	else if (Sub == 2) {
 		cout << "Erm dont you know not to eat random ahh berries ToT" << endl;
